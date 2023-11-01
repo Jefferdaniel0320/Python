71b44c22513135947e69a682b334445d8f83ae83
JefersonRomero <j.daniel0320@gmail.com>
2023-11-01 12:02:14 -0500

    Terminado Nivel 3

---
+++ Ejercicios_Python/herramientasBasicas/comunicandonos.py
@@ -0,0 +1,22 @@
+# Comunicandonos
+print('El dijo: "Hola".')
+print("El dijo: 'Hola'.")
+
+print("a < b = ", "a" < "b")
+print("ab > ac = ", "ab" > "ac")
+print('"Hola" * 3 = ', "hola "*3)
+
+nombre = "Jeferson"
+edad = 34
+
+print(f'Hola, me llamo {nombre}, tengo {edad} años')
+
+print('"34" = ', type(int("34")))
+print("34 = ", type(str(34)))
+
+print("2 + 2 = " + str(2 + int("2")))
+
+edadIntroducida = int(input("Cuantos años tienes? "))
+anio = 2023 - edadIntroducida
+
+print(f"naciste en {anio}?")
