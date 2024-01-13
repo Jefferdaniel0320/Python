44999349154db28975c5c3727de16b87de9291ce
JefersonRomero <j.daniel0320@gmail.com>
2024-01-12 23:55:25 -0500

    ejercicio calendario actualizado

--- Ejercicios_Python/herramientasBasicas/calendario.py
+++ Ejercicios_Python/herramientasBasicas/calendario.py
@@ -2,7 +2,16 @@ import calendar
 
 yy = 1995
 
+
+"""
 for i in range(1,13):
     print(calendar.month(yy, i))
-
+"""
+print("Mes de nacimiento Jeferson")
 print(calendar.month(1989,3))
+print("Mes de nacimiento Olga")
+print(calendar.month(1966,2))
+print("Mes de nacimiento Fernando")
+print(calendar.month(1963,3))
+print("Mes de nacimiento Marco")
+print(calendar.month(1995,3))
